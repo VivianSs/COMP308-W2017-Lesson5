fa7a8c63cdd70c9c926523f06358d22ea01cf70d
Sisi Li <vivianss0301@gmail.com>
2017-02-11 09:47:31 -0500

    restructured project

--- server/app.js
+++ server/app.js
@@ -5,15 +5,14 @@ let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
-// import "mongoose"
+// import "mongoose" NPM module
 let mongoose = require('mongoose');
 
-// URI
-let URI = "mongodb://localhost/videogames";
+// import the config module
+let config = require('./config/db');
 
-//let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
-
-mongoose.connect(URI);
+//connect to the mongdb with the URI above
+mongoose.connect(config.URI);
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+//EXPORT A OBJECT, URI is a property and it has a value
+module.exports = {
+    //local MongoDB
+    "URI": "mongodb://localhost/videogames"
+    // MLab --> "URI" : "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames"
+}
+
+
+
