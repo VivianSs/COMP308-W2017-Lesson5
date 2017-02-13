c4c27426520681c86f0a9da2a2d17e524869c45e
Sisi Li <vivianss0301@gmail.com>
2017-02-13 03:12:05 -0500

    added add and delete functions

--- server/models/games.js
+++ server/models/games.js
@@ -1,7 +1,7 @@
 // import  the mongoose npm package
 let mongoose = require('mongoose');
 
-// create a model class -- specify the document structure
+// create a model class -- specify the  structure
 let gamesSchema = mongoose.Schema({
     name: String,
     rating: Number,
--- server/routes/games.js
+++ server/routes/games.js
@@ -12,21 +12,46 @@ let game = require('../models/games');
 
 /* GET main games page. */
 router.get('/', (req, res, next) => {
-  // find all games in the games collection
+    // find all games in the games collection
 
-  game.find((err, games) => {
+    game.find((err, games) => {
 
-    if (err) {
-      return console.error(err);
-    }
-    else {
-      res.render('games/index', {
-        title: 'Games',
-        games: games
-      });
-    }
-  });
+        if (err) {
+            return console.error(err);
+        }
+        else {
+            res.render('games/index', {
+                title: 'Games',
+                games: games
+            });
+        }
+    });
 });
+
+// GET add page - show the blank details page
+router.get('/add', (req, res, next) => {
+    res.render('games/details', {
+        title: 'Add a new Game',
+        games: ''
+    });
+});
+
+// POST add page -save the Game to the db 
+router.post('/add', (req, res, next) => {
+    game.create({
+        "name": req.body.name,
+        "cost": req.body.cost,
+        "rating": req.body.rating
+    }, (err, game) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            res.redirect('/games');
+        }
+    });
+});
+
 
 /* GET edit -show current game to edit. */
 router.get('/:id', (req, res, next) => {
@@ -34,22 +59,23 @@ router.get('/:id', (req, res, next) => {
     //get a reference to the id of the game to edit
     let id = req.params.id;
 
-  // find the game to edit by it's id  in the games collection
-  game.findById(id, (err, games) => {
+    // find the game to edit by it's id  in the games collection
+    game.findById(id, (err, games) => {
 
-    if (err) {
-      console.error(err);
-      res.end(error);
-    }
-    else {
-        //show the edit view
-      res.render('games/details', {
-        title: 'Game Details',
-        games: games
-      });
-    }
-  });
+        if (err) {
+            console.error(err);
+            res.end(error);
+        }
+        else {
+            //show the edit view
+            res.render('games/details', {
+                title: 'Game Details',
+                games: games
+            });
+        }
+    });
 });
+
 
 /* POST edit -process the  game to edit. */
 router.post('/:id', (req, res, next) => {
@@ -57,7 +83,7 @@ router.post('/:id', (req, res, next) => {
     //get a reference to the id of the game to edit
     let id = req.params.id;
 
-// create a new games objet to hold the changes
+    // create a new games objet to hold the changes
     let games = new game({
         "_id": id,
         "name": req.body.name,
@@ -66,17 +92,36 @@ router.post('/:id', (req, res, next) => {
     });
     //console.log(games);
 
-    game.update({_id: id}, games, (err)=>{
-       if(err){
-           console.log(err);
-           res.end(error);
-       }
-       else{
-           //refresh the game list
-           res.redirect('/games');
-       }
+    game.update({ _id: id }, games, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(error);
+        }
+        else {
+            //refresh the game list
+            res.redirect('/games');
+        }
     });
 });
+
+//GET delete - should delete by id
+router.get('/delete/:id', (req, res, next) => {
+
+    // get a reference to the id of the game to edit
+    let id = req.params.id;
+
+    game.remove({ _id: id }, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            res.redirect('/games');
+        }
+    });
+
+});
+
 
 
 module.exports = router;
